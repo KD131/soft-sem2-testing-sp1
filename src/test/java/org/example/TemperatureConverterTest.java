5eba3e442cd3bbdbc33144e650980fb8959520c3
KD131 <71063280+KD131@users.noreply.github.com>
2023-09-22 21:51:16 +0200

    celsius to fahrenheit

--- src/test/java/org/example/TemperatureConverterTest.java
+++ src/test/java/org/example/TemperatureConverterTest.java
@@ -30,4 +30,46 @@ class TemperatureConverterTest {
         double fahrenheit = 77;
         assertEquals(25, converter.fahrenheitToCelsius(fahrenheit));
     }
+
+    @Test
+    void convert0FtoC() {
+        double fahrenheit = 0;
+        assertEquals(-17.77777777777778, converter.fahrenheitToCelsius(fahrenheit));
+    }
+
+    @Test
+    void convertNegative40CtoF() {
+        double celsius = -40;
+        assertEquals(-40, converter.celsiusToFahrenheit(celsius));
+    }
+
+    @Test
+    void convertNegative30CtoF() {
+        double celsius = -30;
+        assertEquals(-22, converter.celsiusToFahrenheit(celsius));
+    }
+
+    @Test
+    void convert5CtoF() {
+        double celsius = 5;
+        assertEquals(41, converter.celsiusToFahrenheit(celsius));
+    }
+
+    @Test
+    void convert0CtoF() {
+        double celsius = 0;
+        assertEquals(32, converter.celsiusToFahrenheit(celsius));
+    }
+
+    @Test
+    void convert100CtoF() {
+        double celsius = 100;
+        assertEquals(212, converter.celsiusToFahrenheit(celsius));
+    }
+
+    @Test
+    void convert69CtoF() {
+        double celsius = 69;
+        assertEquals(156.2, converter.celsiusToFahrenheit(celsius));
+    }
 }
