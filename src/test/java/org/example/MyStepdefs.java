dd7a420bf6f2d7eb776f93b5f6021de07c3d4aa3
KD131 <71063280+KD131@users.noreply.github.com>
2023-09-25 13:10:50 +0200

    roman numeral scenarios run

--- src/test/java/org/example/MyStepdefs.java
+++ src/test/java/org/example/MyStepdefs.java
@@ -4,16 +4,29 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class MyStepdefs {
+    private static RomanNumeralConverter converter;
+    private static int input;
+    private static String output;
+
     @Given("I have started the converter")
     public void iHaveStartedTheConverter() {
+        converter = new RomanNumeralConverter();
+        assertNotNull(converter);
     }
 
     @When("I convert {int}")
     public void iConvert(int arg0) {
+        input = arg0;
+        output = converter.arabicToRoman(input);
     }
 
-    @Then("I should see I")
-    public void iShouldSeeI() {
+    @Then("I should see {string}")
+    public void iShouldSee(String expected) {
+        assertNotNull(output);
+        assertEquals(expected, output);
     }
 }
--- src/test/resources/features/RomanNumeralConverter.feature
+++ src/test/resources/features/RomanNumeralConverter.feature
@@ -3,4 +3,23 @@ Feature: Convert to Roman numeral
   Scenario: Convert 1 to I
     Given I have started the converter
     When I convert 1
-    Then I should see I
+    Then I should see "I"
+
+  Scenario: Convert 5 to V
+    Given I have started the converter
+    When I convert 5
+    Then I should see "V"
+
+  Scenario Outline: Convert Arabic to Roman
+    Given I have started the converter
+    When I convert <arabic>
+    Then I should see <roman>
+    Examples:
+      | arabic | roman     |
+      | 1      | "I"       |
+      | 5      | "V"       |
+      | 10     | "X"       |
+      | 35     | "XXXV"    |
+      | 996    | "CMXCVI"  |
+      | 1999   | "MCMXCIX" |
+      | 2008   | "MMVIII"  |
